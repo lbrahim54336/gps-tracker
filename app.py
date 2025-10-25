67ef41f7eded3e6b916f41fe7a5e1881b622dd2e
Garba Ibrahim Abdullahi <abclassic360@gmail.com>
2025-10-25 20:31:12 +0100

    update

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def location():
 if __name__ == '__main__':
     # Railway automatically sets a PORT variable
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
+
